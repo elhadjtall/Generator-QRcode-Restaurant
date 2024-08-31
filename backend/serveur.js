9248356bff55e701b32f86ccdc8c7a69f63d3d8f
unknown <elhadjoumartall10@gmail.com>
2024-08-31 21:11:08 +0200

    Creat a project Qr code

---
+++ backend/serveur.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
